c9da4c4a1e5abbc434e89d0d1c5648665c9890a3
wmarcolino <wallacemarcolino@gmail.com>
2023-09-30 12:05:00 -0300

    usando massa de dados

--- cypress/integration/login.spec.cy.js
+++ cypress/integration/login.spec.cy.js
@@ -1,8 +1,9 @@
 ///<reference types="cypress" />
+const perfil = require("../fixtures/perfil.json");
 
 context("Funcionalidade Login", () => {
   beforeEach(() => {
-    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.visit("minha-conta");
   });
 
   afterEach(() => {
@@ -14,10 +15,29 @@ context("Funcionalidade Login", () => {
     cy.get("#password").type("teste@teste.com");
     cy.get(".woocommerce-form > .button").click();
 
+    cy.get(".page-title").should("contain", "Minha conta");
     cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
       "contain",
       "Olá,"
     );
+  });
+
+  it("Deve fazer login com sucesso - Usando arquivo de dados", () => {
+    cy.get("#username").type(perfil.usuario);
+    cy.get("#password").type(perfil.senha);
+    cy.get(".woocommerce-form > .button").click();
+
+    cy.get(".page-title").should("contain", "Minha conta");
+  });
+
+  it.only("Deve fazer login com sucesso - Usando fixture", () => {
+    cy.fixture("perfil").then((dados) => {
+      cy.get("#username").type(dados.usuario);
+      cy.get("#password").type(dados.senha, { log: false });
+      cy.get(".woocommerce-form > .button").click();
+
+      cy.get(".page-title").should("contain", "Minha conta");
+    });
   });
 
   it("Deve exibir uma mensagem de erro ao inserir usuário invalido", () => {
--- cypress/integration/pre-cadastro.spec.cy.js
+++ cypress/integration/pre-cadastro.spec.cy.js
@@ -3,7 +3,7 @@ import { faker } from "@faker-js/faker";
 
 describe("Funcionalidade Pre Cadastro", () => {
   beforeEach(() => {
-    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.visit("minha-conta");
   });
 
   it("Deve completar o pré cadastro com sucesso", () => {
